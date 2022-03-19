505e38ec26de01b2392a4cbb3e044ea2db0476b3
Stephen Ross <stephenr.ross@yahoo.com>
2022-03-19 18:02:14 +0000

    [ci skip] new deploy test

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# new ssh key pair set up on server and public added to gitlab profile
+# server repos updated to ssh urls using new key pair to connect without password
+
 cd ./project/backend
 git fetch origin
 git pull origin
+
 
 cd ../frontend
 git fetch origin
